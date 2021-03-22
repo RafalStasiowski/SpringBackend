950c10a3fa2a0b731d3f98ababa7f2939ea6d6bf
Rafal <rafal.tomasz.stasiowsko@gmail.com>
2021-03-22 13:53:12 +0100

    Added User model and included loombok

---
+++ src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,31 @@
+package com.example.backend.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String LastName;
+    @Email
+    @NotBlank
+    private String email;
+    private boolean active;
+}
